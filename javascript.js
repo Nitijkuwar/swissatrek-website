7dde2df71454a004d0bc092eefcb4b68db7805dc
NitijKuwar <nitijkuwar555@gmail.com>
2025-02-12 15:59:55 +0545

    about section created

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// navbar
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+        } else {
+            x.className = "navbar";
+        }
+    }
+</script>
