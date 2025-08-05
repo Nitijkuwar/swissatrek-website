6ad35a2a957af691135ebacc2542de4d1345c224
NitijKuwar <nitijkuwar555@gmail.com>
2025-08-05 15:43:34 +0545

    added home page

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 // navbar
-<script>
-    function myFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "navbar") {
-            x.className += " responsive";
-        } else {
-            x.className = "navbar";
-        }
-    }
-</script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+document.getElementById("scrollToTop").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
